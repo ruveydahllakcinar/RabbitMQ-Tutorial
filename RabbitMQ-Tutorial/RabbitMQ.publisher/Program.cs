b91d675d1a0d122d819a517a2754a653a72bc9f5
Ruveyda Hilal <rhakcinar@gmail.com>
2023-08-20 15:37:18 +0300

    I talked about the basic subjects of RabbitMQ and what is the exchange, what are the types.

---
+++ RabbitMQ-Tutorial/RabbitMQ.publisher/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using Shared;
+using System.Text;
+using System.Text.Json;
+
+public class Program
+{
+    public enum LogNames
+    {
+        Critical=1,
+        Error=2,
+        Warning=3,
+        Info=4
+
+    }
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://jamopzbo:wr6fN8dWljpKl8RsQb0Qc7mfEbGADjuV@moose.rmq.cloudamqp.com/jamopzbo");
+
+        using var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+
+        channel.ExchangeDeclare("header-exchange",durable:true,type:ExchangeType.Headers);
+
+        Dictionary<string,object> headers = new Dictionary<string,object>();
+
+        headers.Add("format", "pdf");
+        headers.Add("shape2", "A4");
+
+        var properties = channel.CreateBasicProperties();
+        properties.Headers = headers;
+        properties.Persistent = true; //Mesajlar kalıcı hale gelir.
+
+        var product = new Product
+        {
+            Id = 1,
+            Name = "Book",
+            Price = 250,
+            Stock = 2
+        };
+
+        var productJsonString = JsonSerializer.Serialize(product);
+
+
+
+
+        channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
+        Console.WriteLine("Message has been sended");
+
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQ-Tutorial/RabbitMQ.subscribe/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shared;
+using System;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://jamopzbo:wr6fN8dWljpKl8RsQb0Qc7mfEbGADjuV@moose.rmq.cloudamqp.com/jamopzbo");
+
+        using var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+        channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+        channel.BasicQos(0, 1, false);
+        var consumer = new EventingBasicConsumer(channel);
+        var queueName = channel.QueueDeclare().QueueName;
+        Dictionary<string, object> headers = new Dictionary<string, object>();
+
+        headers.Add("format", "pdf");
+        headers.Add("shape", "A4");
+        headers.Add("x-match", "any");
+        channel.QueueBind(queueName, "header-exchange",String.Empty,headers);
+        channel.BasicConsume(queueName, false, consumer);
+        Console.WriteLine("Listening logs...");
+
+        consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Product product = JsonSerializer.Deserialize<Product>(message);
+            Thread.Sleep(1500);
+            Console.WriteLine($"Incoming message: { product?.Id} - {product?.Name}- {product?.Price}- {product?.Stock}");
+
+            
+            channel.BasicAck(e.DeliveryTag, false);
+        };
+        Console.ReadLine();
+    }
+
+}
+
